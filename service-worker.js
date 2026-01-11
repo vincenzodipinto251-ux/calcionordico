a681562d9007d699d457d9e8a60eec58ac5e7d0c
vincenzodipinto251-ux <vincenzodipinto251@gmail.com>
2026-01-11 09:55:33 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE = "calcio-nordico-v1";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
